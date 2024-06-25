e57b9ac2c0cdf9c2bf25d725cd543bdb584dabda
Tauan Camargo <lukyhenson@gmail.com>
2024-06-24 22:53:48 -0300

    Initial Commit

---
+++ WarGame/WarGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarGameApp.swift
+//  WarGame
+//
+//  Created by Tauan Tathiell Camargo on 24/06/24.
+//
+
+import SwiftUI
+
+@main
+struct WarGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
